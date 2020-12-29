26aa9a9ba856a687eb6c0d6ec6ab0f5161003071
Mor Fall <fall.mor@outlook.com>
2020-12-29 12:11:34 +0100

    fix go mod

--- project-golang/go.mod
+++ project-golang/go.mod
@@ -1,3 +1,5 @@
 module handlers
 
 go 1.15
+
+require github.com/fallmor/cours-go v0.0.0-20201229111134-1a57dadb39d8 // indirect
--- project-golang/microservice.go
+++ project-golang/microservice.go
@@ -1,35 +1,65 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
-	"github.com/fallmor/cours-go/tree/main/project-golang/handlers"
+	"github.com/fallmor/cours-go/project-golang/handlers"
+	"github.com/nicholasjackson/env"
 )
+
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
 
 func main() {
 
-	/*http.HandleFunc("/", func(wr http.ResponseWriter, r *http.Request) {
-	 		//cette fonction va recevoir les requestes http (HandleFunc qui prend deux arguments le chemin, et une fonction )
-			log.Println("Hello from Mor")
-			d, err := ioutil.ReadAll(r.Body)
-			//log.Printf("data %s\n", d) //affiche les requetes passées avec curl
-			fmt.Fprintf(wr, "Hello %s", d) //affiche les reponses avec le wr qui represente http.ResponseWriter
-			if err != nil {
-				//wr.WriteHeader(http.StatusBadGateway) //Go permet de gerer les codes http telque 404 de manière très simple avec le package http
-				//wr.Write([]byte("Ooops"))             // le message qui sera affiché avec le status code
-				http.Error(wr, "Ooops", http.StatusBadGateway)
-				return
-			}
-		})
-		http.HandleFunc("/Bye", func(http.ResponseWriter, *http.Request) {
-			log.Println("ByeBye from Mor")
-		})*/ //le code handler est géré maintenant par le fichier hello.go qui est dans le dossier handler
+	env.Parse()
 
-	l := log.New(os.Stdout, "Product-Api", log.LstdFlags)
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+
+	// create the handlers
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	// create a new serve mux and register the handlers
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
-	http.ListenAndServe(":9090", sm) //accepte toutes les connexions sur le port 9090. En quelque sorte on crée un serveur http
+	sm.Handle("/goodbye", gh)
+
+	// create a new server
+	s := http.Server{
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	// start the server
+	go func() {
+		l.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
