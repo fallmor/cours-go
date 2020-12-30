63ab1fff95057175a93f09c61a9da100eb32e1e7
Mor Fall <fall.mor@outlook.com>
2020-12-30 14:08:37 +0100

    finally finished the REST Api with golang

--- project-golang/data/product.go
+++ project-golang/data/product.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -38,6 +39,26 @@ func AddProducts(p *Product) {
 func GetNextId() int {
 	lp := productList[len(productList)-1]
 	return lp.ID + 1
+}
+func UpdateProducts(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+var ErrorProductNotfound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrorProductNotfound
 }
 
 var productList = []*Product{
--- project-golang/handlers/products.go
+++ project-golang/handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/fallmor/cours-go/project-golang/data"
 )
@@ -22,6 +24,27 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	}
 	if r.Method == http.MethodPost {
 		p.addProduct(rw, r)
+	}
+	if r.Method == http.MethodPut {
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(g) != 1 {
+			p.l.Println("Invalid URI request more than one ID")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI request more than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI unable to convert to numer", idString)
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		p.l.Println("Update request pass ", id)
+		p.UpdateProducts(id, rw, r)
 	}
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 
@@ -51,4 +74,22 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	}
 	p.l.Printf("Prod: %#v", prod)
 	data.AddProducts(prod)
+}
+func (p *Products) UpdateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle Post Products")
+	prod := &data.Product{}
+	err := prod.FromJ(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to use Post Method", http.StatusBadRequest)
+	}
+	p.l.Printf("Prod: %#v", prod)
+	err = data.UpdateProducts(id, prod)
+	if err == data.ErrorProductNotfound {
+		http.Error(rw, "Product Not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
 }
